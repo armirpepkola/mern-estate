1b16d5c0fe80ea7e630dd20445e7f6047e86b11f
armirpepkola <armirpepkola@outlook.com>
2024-01-23 15:21:21 +0100

    models

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -1,63 +1,80 @@
 import mongoose from 'mongoose';
 
+// Define the schema for the Listing model
 const listingSchema = new mongoose.Schema(
     {
+        // Name of the listing
         name: {
         type: String,
         required: true,
         },
+        // Description of the listing
         description: {
         type: String,
         required: true,
         },
+        // Address of the listing
         address: {
         type: String,
         required: true,
         },
+        // Regular price of the listing
         regularPrice: {
         type: Number,
         required: true,
         },
+        // Discounted price of the listing
         discountPrice: {
         type: Number,
         required: true,
         },
+        // Number of bathrooms in the listing
         bathrooms: {
         type: Number,
         required: true,
         },
+        // Number of bedrooms in the listing
         bedrooms: {
         type: Number,
         required: true,
         },
+        // Indicates if the listing is furnished or not
         furnished: {
         type: Boolean,
         required: true,
         },
+        // Indicates if parking is available for the listing
         parking: {
         type: Boolean,
         required: true,
         },
+        // Type of the listing (e.g., sale or rent)
         type: {
         type: String,
         required: true,
         },
+        // Indicates if there is an offer on the listing
         offer: {
         type: Boolean,
         required: true,
         },
+        // Array of image URLs for the listing
         imageUrls: {
         type: Array,
         required: true,
         },
+        // Reference to the user who created the listing
         userRef: {
         type: String,
         required: true,
         },
     },
+    // Add timestamps to track creation and modification dates
     { timestamps: true }
 );
 
+// Create the Listing model using the schema
 const Listing = mongoose.model('Listing', listingSchema);
 
+// Export the Listing model
 export default Listing;
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,29 +1,37 @@
 import mongoose from 'mongoose';
 
+// Define the schema for the User model
 const userSchema = new mongoose.Schema(
   {
+    // Username of the user, must be unique
     username: {
       type: String,
       required: true,
       unique: true,
     },
+    // Email of the user, must be unique
     email: {
       type: String,
       required: true,
       unique: true,
     },
+    // Password of the user
     password: {
       type: String,
       required: true,
     },
-    avatar:{
+    // Avatar URL of the user, default to a placeholder image
+    avatar: {
       type: String,
       default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
   },
+  // Add timestamps to track creation and modification dates
   { timestamps: true }
 );
 
+// Create the User model using the schema
 const User = mongoose.model('User', userSchema);
 
+// Export the User model
 export default User;
