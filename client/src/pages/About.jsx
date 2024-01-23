7a2dac1f60500ee4fe81b7cce8112f2acc4c315f
armirpepkola <armirpepkola@outlook.com>
2024-01-23 13:14:54 +0100

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Real Estate Marketplace</h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam, enim in sollicitudin bibendum, libero ex cursus lectus, eget mollis nisi mi vitae mi. Ut dapibus vel orci non consequat. Cras ullamcorper lorem vitae nunc varius, ac rutrum tellus maximus. Morbi non orci vitae mi feugiat gravida. Phasellus posuere nisi odio, blandit dictum augue porta ac. Donec a elit in enim finibus gravida. Morbi malesuada turpis ligula, vel consequat sem venenatis eu. Donec id nulla id enim fringilla tincidunt. Cras condimentum ac erat nec pulvinar. Quisque dolor justo, aliquet consectetur ultricies quis, tristique nec nisl. Maecenas a accumsan sem. Curabitur vel mauris varius, lacinia justo sit amet, dapibus diam. Vestibulum luctus aliquam ultricies.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Fusce malesuada ac nulla a blandit. Mauris hendrerit consequat eros ut imperdiet. Ut sagittis, quam vel gravida tincidunt, diam sapien varius risus, euismod blandit arcu tortor vel ex. Phasellus mi lorem, molestie in turpis a, eleifend dignissim tortor. Donec efficitur in metus id vehicula. Curabitur dictum cursus condimentum. Pellentesque consequat diam eu semper interdum.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Aenean vitae lacinia nisl, et lacinia nunc. Mauris non imperdiet metus. Proin sed eleifend velit. Aenean eget aliquet augue, non consectetur dui. Ut interdum imperdiet purus sit amet auctor. Praesent eu consequat leo. Etiam dictum porta mi, et pellentesque neque consectetur a.
+      </p>
+    </div>
   )
 }
