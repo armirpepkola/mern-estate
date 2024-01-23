ed1b36b36ad819b1e8e01e9cfd546cce01de0d07
armirpepkola <armirpepkola@outlook.com>
2024-01-23 15:20:18 +0100

    utils

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,14 @@
+// Utility function to create an error object with a specific status code and message
 export const errorHandler = (statusCode, message) => {
+    // Create a new Error object
     const error = new Error();
+
+    // Set the status code of the error
     error.statusCode = statusCode;
+
+    // Set the error message
     error.message = message;
+
+    // Return the error object
     return error;
 };
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,15 +1,26 @@
+// Utility function to verify a user's access token
 import jwt from 'jsonwebtoken';
+
+// Import the errorHandler utility function
 import { errorHandler } from './error.js';
 
+// Middleware function to verify the access token in the request cookies
 export const verifyToken = (req, res, next) => {
+  // Extract the access token from the request cookies
   const token = req.cookies.access_token;
 
+  // If no token is found, return an unauthorized error
   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
+  // Verify the token using the JWT_SECRET from the environment variables
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    // If an error occurs during token verification, return a forbidden error
     if (err) return next(errorHandler(403, 'Forbidden'));
 
+    // Attach the user information to the request for further use in controllers
     req.user = user;
+
+    // Continue to the next middleware or route handler
     next();
   });
 };
